513db0f2f82a1a742e0d96128116ce0187ccec9f
vladan345 <vladanm087@gmail.com>
2023-11-27 16:03:09 +0100

    finisheeed (auth not working tho)

--- auth.config.ts
+++ auth.config.ts
@@ -6,7 +6,9 @@ export const authConfig = {
   },
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
+      console.log(auth);
       const isLoggedIn = !!auth?.user;
+
       const isOnDashboard = nextUrl.pathname.startsWith("/dashboard");
       if (isOnDashboard) {
         if (isLoggedIn) return true;
